15e3ca90ca63017fd197404d22ff04f8bbb8799f
Frederick Price <rprice@pricemail.ca>
2025-06-11 16:15:12 -0400

    Current status

--- build.rs
+++ build.rs
@@ -1,5 +1,3 @@
-use pkg_config;
-
 fn main() {
     if pkg_config::Config::new().probe("x11").is_err() {
         panic!("X11 development libraries not found. Please install libx11-dev or xorg-x11-devel");
